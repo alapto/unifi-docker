854d24e1a3ab73bf8a17652e3c4d10a875a777d9
Toni Ala-Piirto <alapto@gmail.com>
2018-07-07 11:34:24 +0300

    Disable mongo installation

--- docker-build.sh
+++ docker-build.sh
@@ -37,12 +37,12 @@ apt-get install -qy --no-install-recommends \
     procps \
     libcap2-bin
 tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+# echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
 echo "deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti" > /etc/apt/sources.list.d/20ubiquiti.list
 tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv C0A52C50
 curl -L -o ./unifi.deb "${1}"
-apt -qy install mongodb-org ./unifi.deb
+apt -qy install ./unifi.deb
 rm -f ./unifi.deb
 chown -R unifi:unifi /usr/lib/unifi
 rm -rf /var/lib/apt/lists/*
